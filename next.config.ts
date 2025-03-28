1fc5e00653d7d69cb8106c412a93f5f4b87e7e98
Genichi Ishii <gishii507@gmail.com>
2025-03-28 22:05:22 +0900

    next.config.tsにnext-remove-importsプラグインを追加し、不要なインポートを削除する設定を追加。package.jsonとpackage-lock.jsonに新しい依存関係を追加し、src/app/page.tsxでMarkdownエディタを動的にインポートして使用するように変更。

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+import removeImports from 'next-remove-imports';
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+const removeImportsPlugin = removeImports();
+
+export default removeImportsPlugin({
+  ...nextConfig,
+});
